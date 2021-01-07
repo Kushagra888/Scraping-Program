30acdf7252a09bd519df8a0e73634939193443b5
Kushagra <kushagrachavel@gmail.com>
2021-01-07 22:40:03 +0530

    almost completed

--- Products/spiders/Bitfang.py
+++ Products/spiders/Bitfang.py
@@ -19,7 +19,7 @@ from selenium.webdriver.support import expected_conditions as EC
 class BitfangSpider(scrapy.Spider):
     name = 'Bitfang'
  
-    start_urls = ['https://www.amazon.in']
+    start_urls = ['https://www.bitfang.com']
  
     def __init__(self, product_names=None, low_prices=None, high_prices=None, brand_names=None, conditions=None, product_filters=None, *args, **kwargs):
         super(BitfangSpider, self).__init__(*args, **kwargs)
@@ -28,8 +28,8 @@ class BitfangSpider(scrapy.Spider):
  
         for i in range(len(product_names)):
  
-            chrome_options = Options()
-            chrome_options.add_argument('--headless')
+            # chrome_options = Options()
+            # chrome_options.add_argument('--headless')
  
             chrome_path = which('chromedriver')
  
@@ -37,7 +37,7 @@ class BitfangSpider(scrapy.Spider):
  
             driver.set_window_size(1920, 1080)
  
-            driver.get('https://www.amazon.in')
+            driver.get('https://www.bitfang.com')
  
             if product_names[i] == '' or product_names[i] == None:
                 raise CloseSpider('Please enter a product name.')
@@ -54,188 +54,52 @@ class BitfangSpider(scrapy.Spider):
             try:
                 element = WebDriverWait(driver, 5).until(
                     EC.presence_of_element_located(
-                        (By.ID, "twotabsearchtextbox"))
+                        (By.XPATH, "//input[@class='search-box-text']"))
                 )
                 if element == True:
                     pass
             except:
                 pass
             finally:
-                input_box = driver.find_element_by_id('twotabsearchtextbox')
+                input_box = driver.find_element_by_xpath("//input[@class='search-box-text']")
                 input_box.send_keys(product_names[i])
                 input_box.send_keys(Keys.ENTER)
- 
-            if high_prices[i] == 0 or high_prices[i] == '0':
-                raise CloseSpider('High price value cannot be zero!!!')
- 
-            if low_prices[i] != None:
-                try:
-                    low = int(low_prices[i])
-                except ValueError:
-                    raise CloseSpider('Low Price value must be an integer!')
- 
-            if high_prices[i] != None:
-                try:
-                    high = int(high_prices[i])
-                except ValueError:
-                    raise CloseSpider('High Price value must be an integer!')
- 
-            try:
-                element1 = WebDriverWait(driver, 5).until(
-                    EC.presence_of_element_located((By.ID, "low-price"))
-                )
-                element2 = WebDriverWait(driver, 5).until(
-                    EC.presence_of_element_located((By.ID, "high-price"))
-                )
-                if element1 == True or element2 == True:
-                    pass
-            except:
-                pass
-            finally:
-                if low_prices[i] != None:
-                    self.low_pr = driver.find_element_by_id('low-price')
-                    self.low_pr.send_keys(low)
- 
-                if high_prices[i] != None:
-                    self.high_pr = driver.find_element_by_id('high-price')
-                    self.high_pr.send_keys(high)
- 
-                if high_prices[i] == None and low_prices[i] != None:
-                    self.low_pr.send_keys(Keys.ENTER)
- 
-                elif low_prices[i] == None and high_prices[i] != None:
-                    self.high_pr.send_keys(Keys.ENTER)
- 
-                elif low_prices[i] != None and high_prices[i] != None:
-                    self.high_pr.send_keys(Keys.ENTER)
- 
-            try:
-                element1 = WebDriverWait(driver, 5).until(
-                    EC.element_to_be_clickable(
-                        (By.XPATH, "(//span[contains(text(),'Item Condition')]/following::span[@class='a-size-base a-color-base'])[contains(text(),'New')]"))
-                )
-                element2 = WebDriverWait(driver, 5).until(
-                    EC.element_to_be_clickable(
-                        (By.XPATH, "(//span[contains(text(),'Item Condition')]/following::span[@class='a-size-base a-color-base'])[contains(text(),'Used')]"))
-                )
-                if element1 == True or element2 == True:
-                    pass
-            except:
-                pass
-            finally:
-                if conditions[i] != None:
-                    if conditions[i].lower() == 'new':
-                        New = driver.find_element_by_xpath(
-                            "(//span[contains(text(),'Item Condition')]/following::span[@class='a-size-base a-color-base'])[contains(text(),'New')]")
-                        New.click()
- 
-                    elif conditions[i].lower() == 'used':
-                        Used = driver.find_element_by_xpath(
-                            "(//span[contains(text(),'Item Condition')]/following::span[@class='a-size-base a-color-base'])[contains(text(),'Used')]")
-                        Used.click()
- 
-                    else:
-                        raise CloseSpider(
-                            'Please enter a valid Item Condition! or do not pass any condition if you do not want any condition')
- 
+           
             if product_filters[i] != None:
-                if product_filters[i].lower() == 'featured':
+                if product_filters[i].lower() == 'relevance':
                     pass
  
             try:
                 elementA = WebDriverWait(driver, 5).until(
                     EC.presence_of_element_located(
-                        (By.ID, "s-result-sort-select"))
+                        (By.XPATH, "//div[@class='sortbydiv']/a"))
                 )
-                elementB = WebDriverWait(driver, 5).until(
-                    EC.element_to_be_clickable((By.ID, "s-result-sort-select"))
-                )
-                if elementA or elementB == True:
+
+                if elementA == True:
                     pass
-                if product_filters[i] != None and product_filters[i] != 'featured':
+                if product_filters[i] != None and product_filters[i] != 'relevance':
                     if product_filters[i].lower() == 'low to high':
-                        sort_btn = driver.find_element_by_id(
-                            's-result-sort-select')
-                        sort_btn.send_keys(Keys.ENTER)
- 
                         low_high = driver.find_element_by_id(
-                            's-result-sort-select_1')
+                            'btnLowPrice')
                         low_high.click()
  
                     if product_filters[i].lower() == 'high to low':
-                        sort_btn = driver.find_element_by_id(
-                            's-result-sort-select')
-                        sort_btn.send_keys(Keys.ENTER)
- 
                         high_low = driver.find_element_by_id(
-                            's-result-sort-select_2')
+                            'btnHighPrice')
                         high_low.click()
  
-                    if product_filters[i].lower() == 'average customer review':
-                        sort_btn = driver.find_element_by_id(
-                            's-result-sort-select')
-                        sort_btn.send_keys(Keys.ENTER)
+                    if product_filters[i].lower() == 'popular':
+                        popu = driver.find_element_by_id(
+                            'btnPopular')
+                        popu.click()
  
-                        avg_rv = driver.find_element_by_id(
-                            's-result-sort-select_3')
-                        avg_rv.click()
- 
-                    if product_filters[i].lower() == 'newest arrivals':
-                        sort_btn = driver.find_element_by_id(
-                            's-result-sort-select')
-                        sort_btn.send_keys(Keys.ENTER)
- 
-                        new_ar = driver.find_element_by_id(
-                            's-result-sort-select_4')
-                        new_ar.click()
+                    if product_filters[i].lower() == 'new':
+                        new = driver.find_element_by_id(
+                            'btnNew')
+                        new.click()
  
             except Exception:
                 pass
- 
-            # For Brand Name:-
-            if brand_names[i] != None:
-                pg_src = driver.page_source
-                resp = Selector(text=pg_src)
- 
-                pro_names = []
-                pro_num = []
- 
-                try:
-                    names_list = resp.xpath(
-                        "//span[contains(text(),'Brand')]/following::div[@class='a-checkbox a-checkbox-fancy s-navigation-checkbox aok-float-left']/following::span[1]/text()").getall()
- 
-                    for nm in names_list:
-                        pro_names.append(nm)
- 
-                    for s in range(1, len(pro_names)+1):
-                        pro_num.append(s)
- 
-                    pro_dict = dict(zip(pro_names, pro_num))
- 
-                    n = pro_dict.get(brand_names[i])
- 
-                except Exception:
-                    raise CloseSpider(
-                        'Please enter a valid brand name! or do not pass any brand name if you do not want any brand name.')
- 
-                try:
-                    WebDriverWait(driver, 5).until(
-                        EC.element_to_be_clickable(
-                            (By.XPATH, f"(//span[contains(text(),'Brand')]/following::div[@class='a-checkbox a-checkbox-fancy s-navigation-checkbox aok-float-left'])[{n}]"))
-                    )
-                    
-                    try:
-                        see_more_btn = driver.find_element_by_xpath("(//span[@class='a-expander-prompt'])[2]")
-                        see_more_btn.click()
-                    except:
-                        pass
-
-                    check_box = driver.find_element_by_xpath(
-                        f"(//span[contains(text(),'Brand')]/following::div[@class='a-checkbox a-checkbox-fancy s-navigation-checkbox aok-float-left'])[{n}]")
-                    check_box.click()
- 
-                except:
-                    pass
  
             self.page_sources.append(driver.page_source)
  
@@ -247,7 +111,7 @@ class BitfangSpider(scrapy.Spider):
             resp = Selector(text=html)
  
             links = resp.xpath(
-                "//div[@class='a-section a-spacing-none']/h2/a/@href").getall()
+                "//div[@class='picture']/a/@href").getall()
  
             for link in links:
                 url = response.urljoin(link)
@@ -266,13 +130,13 @@ class BitfangSpider(scrapy.Spider):
         ID = response.meta.get('ID')
  
         Product_name = response.xpath(
-            'normalize-space(//span[@id="productTitle"]/text())').get()
+            'normalize-space(//h1[@itemprop="name"]/text())').get()
  
         Product_price = self.remove_char(response.xpath(
-            '//td[@class="a-span12"]/span[1]/text()').get())
+            "normalize-space((//span[@itemprop='price']/text())[2])").get())
  
         pd_img_url = response.xpath(
-            'normalize-space(//div[@id="imgTagWrapperId"]/img/@src)').get()
+            'normalize-space(//div[@class="picture"]/img[@itemprop="image"]/@src)').get()
  
         Product_link = response.url
  
